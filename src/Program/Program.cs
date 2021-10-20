867142fba3478b2bb9ea1722e6bbe0236bc53b16
guilleee <88690005+guilleee@users.noreply.github.com>
2021-10-19 22:42:22 -0300

    Ejercicio2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,15 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"C:\Users\guill\Desktop\Pipes Filters1\PipesFilters1\src\Program\beer.jpg");
+            IPipe nulo = new PipeNull();
+            IFilter negativo = new FilterNegative();
+            IPipe primerserial = new PipeSerial(negativo,nulo);
+            IFilter gris = new FilterGreyscale();
+            IPipe segundoserial = new PipeSerial(gris, primerserial);
+            IPicture final = segundoserial.Send(picture);
+            provider.SavePicture(final, @"C:\Users\guill\Desktop\Pipes Filters1\PipesFilters1\src\Program\save.jpg");
         }
     }
 }
